c82b51fb3dd7c8d03c41331d8327c4931939efe1
schmij03 <96172389+schmij03@users.noreply.github.com>
2023-05-01 15:19:05 +0200

    created springboot application

---
+++ playgroundjan/src/main/java/com/schmij03/project2mdm/playgroundjan/PlaygroundjanApplication.java
@@ -0,0 +1,13 @@
+package com.schmij03.project2mdm.playgroundjan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaygroundjanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaygroundjanApplication.class, args);
+	}
+
+}
---
+++ playgroundjan/src/test/java/com/schmij03/project2mdm/playgroundjan/PlaygroundjanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.schmij03.project2mdm.playgroundjan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaygroundjanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
