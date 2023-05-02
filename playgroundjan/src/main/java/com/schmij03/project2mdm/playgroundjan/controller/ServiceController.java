f75c05b4d2215dcf876590eacee26b67161a7b6d
schmij03 <96172389+schmij03@users.noreply.github.com>
2023-05-02 16:26:44 +0200

    changed the files

--- playgroundjan/src/main/java/com/schmij03/project2mdm/playgroundjan/controller/ServiceController.java
+++ playgroundjan/src/main/java/com/schmij03/project2mdm/playgroundjan/controller/ServiceController.java
@@ -1,23 +1,34 @@
 package com.schmij03.project2mdm.playgroundjan.controller;
 
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.multipart.MultipartFile;
-
 import com.schmij03.project2mdm.playgroundjan.model.ImageClassifier;
+
+import java.io.IOException;
 
 @RestController
 public class ServiceController {
 
     private ImageClassifier classifier = new ImageClassifier();
 
+    @GetMapping("/")
+    public ModelAndView index() {
+        return new ModelAndView("index.html");
+    }
+
+    @GetMapping("/ping")
+    public String ping() {
+        return "Image classification app is up and running!";
+    }
+
     @PostMapping("/classify")
-    public String classify(@RequestParam("file") MultipartFile file) throws Exception {
-        if (file.isEmpty()) {
-            throw new IllegalArgumentException("The file is empty");
-        }
-        var result = classifier.classify(file.getInputStream());
+    public String classify(@RequestParam(name = "file", required = true) MultipartFile file) throws IOException {
+        var result = classifier.classify(file);
         return result.toJson();
     }
 }
--- playgroundjan/src/main/java/com/schmij03/project2mdm/playgroundjan/model/ImageClassifier.java
+++ playgroundjan/src/main/java/com/schmij03/project2mdm/playgroundjan/model/ImageClassifier.java
@@ -1,46 +1,66 @@
 package com.schmij03.project2mdm.playgroundjan.model;
 
+import org.apache.mxnet.javaapi.*;
+import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.StandardCopyOption;
-
-import org.slf4j.LoggerFactory;
-
-import ai.djl.Application;
-import ai.djl.Device;
-import ai.djl.MalformedModelException;
 import ai.djl.inference.Predictor;
-import ai.djl.modality.Classifications;
-import ai.djl.repository.zoo.Criteria;
-import ai.djl.repository.zoo.ModelNotFoundException;
-import ai.djl.repository.zoo.ModelZoo;
-import ai.djl.repository.zoo.ZooModel;
-import ai.djl.training.util.ProgressBar;
-import ai.djl.translate.TranslateException;
-import ch.qos.logback.classic.Logger;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 
 public class ImageClassifier {
 
-    private static final Logger logger = (Logger) LoggerFactory.getLogger(ImageClassifier.class);
+    private static final String MODEL_PATH = "resnet50_v2";
+    private static final String SYNSET_PATH = "synset.txt";
 
-    public ImageClassifier() {
-    }
+    private final Predictor predictor;
+    private final String[] synset;
 
-    public Classifications classify(InputStream imageInputStream) throws MalformedModelException, ModelNotFoundException, IOException, TranslateException {
-        logger.info("Classifying image...");
-
-        Criteria<InputStream, Classifications> criteria = Criteria.builder().setTypes(InputStream.class, Classifications.class)
-                .optApplication(Application.CV.IMAGE_CLASSIFICATION).optDevice(Device.cpu()).optFilter("backbone", "resnet50")
-                .optFilter("dataset", "imagenet").optProgress(new ProgressBar()).build();
-
-        Path tempImage = Files.createTempFile("temp-image", ".jpg");
-        Files.copy(imageInputStream, tempImage, StandardCopyOption.REPLACE_EXISTING);
-        try (ZooModel<InputStream, Classifications> model = ModelZoo.loadModel(criteria);
-                Predictor<InputStream, Classifications> predictor = model.newPredictor()) {
-            return predictor.predict(Files.newInputStream(tempImage));
+    public ImageClassifier() throws IOException {
+        String modelDirPath = System.getProperty("user.dir") + "/models";
+        File modelDir = new File(modelDirPath);
+        if (!modelDir.exists()) {
+            modelDir.mkdir();
         }
+
+        // Load the ResNet50_v2 model
+        String modelPath = modelDirPath + "/" + MODEL_PATH;
+        Symbol symbol = Symbol.load(modelPath + "-symbol.json");
+        DataIter dataIter = ImageRecordIter.newBuilder()
+                .setPath(modelDirPath + "/val.rec")
+                .setBatchSize(1)
+                .build();
+        predictor = new Predictor(symbol, new NDListManager(dataIter));
+
+        // Load the synset
+        String synsetPath = modelDirPath + "/" + SYNSET_PATH;
+        synset = Files.lines(new File(synsetPath).toPath())
+                .map(String::trim)
+                .toArray(String[]::new);
     }
+
+    public ClassificationResult classify(MultipartFile file) throws IOException {
+        // Convert MultipartFile to File
+        File tempFile = File.createTempFile("temp", null);
+        file.transferTo(tempFile);
+
+        // Preprocess the image
+        NDArray input = Image.imdecode(Files.readAllBytes(tempFile.toPath()), Imgcodecs.IMREAD_COLOR);
+        input = Image.resize(input, new Shape(224, 224));
+        input = input.transpose(new Shape(2, 0, 1)).expandDims(0).div(255);
+
+        // Run the prediction
+        NDList output = predictor.predict(new NDList(input));
+        NDArray probabilities = SoftmaxActivation.apply(output.singletonOrThrow());
+        float[] probabilitiesArray = probabilities.toFloatArray();
+        int predictedClass = probabilities.argMax().toIntArray()[0];
+
+        // Clean up the temporary file
+        tempFile.delete();
+
+        // Return the classification result
+        return new ClassificationResult(synset[predictedClass], probabilitiesArray[predictedClass]);
+    }
+
 }
